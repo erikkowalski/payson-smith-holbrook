08a7971ea22e75e00ff42784453ae119c796e15e
erikkowalski <erikkowalski@mac.com>
2015-06-08 15:30:25 -0400

    Added Carousel to home page

--- front-page.php
+++ front-page.php
@@ -1,5 +1,39 @@
-<h1>front page</h1>
 <?php while (have_posts()) : the_post(); ?>
-<?php get_template_part('templates/page', 'header'); ?>
+<?php // get_template_part('templates/page', 'header'); ?>
+<div id="carousel-home" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-home" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-home" data-slide-to="1"></li>
+        <li data-target="#carousel-home" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="<?php the_field('home_slider_image_one'); ?>" alt="slide one">
+            <div class="carousel-caption">
+
+            </div>
+        </div>
+        <!--<div class="item">
+            <img src="..." alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div> -->
+
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
