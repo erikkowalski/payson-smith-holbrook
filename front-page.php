7a873a33a2567a0f66139299416b6ad155e483ec
erikkowalski <erikkowalski@mac.com>
2015-06-08 17:22:38 -0400

    Added fade transitions to  home carousel slideer

--- front-page.php
+++ front-page.php
@@ -1,11 +1,12 @@
 <?php while (have_posts()) : the_post(); ?>
 <?php // get_template_part('templates/page', 'header'); ?>
-<div id="carousel-home" class="carousel slide" data-ride="carousel">
+<div id="carousel-home" class="carousel fade " data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
         <li data-target="#carousel-home" data-slide-to="0" class="active"></li>
         <li data-target="#carousel-home" data-slide-to="1"></li>
         <li data-target="#carousel-home" data-slide-to="2"></li>
+        <li data-target="#carousel-home" data-slide-to="3 "></li>
     </ol>
 
     <!-- Wrapper for slides -->
@@ -16,24 +17,28 @@
 
             </div>
         </div>
-        <!--<div class="item">
-            <img src="..." alt="...">
-            <div class="carousel-caption">
-                ...
-            </div>
-        </div> -->
+       <div class="item">
+            <img src="<?php the_field('home_slider_image_two'); ?>" alt="slide two">
+        </div>
+    <div class="item">
+            <img src="<?php the_field('home_slider_image_three'); ?>" alt="slide three">
+        </div>
+           <div class="item">
+            <img src="<?php the_field('home_slider_image_four'); ?>" alt="slide four">
+        </div>
+
 
     </div>
 
     <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
+    <!--<a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
     <a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
         <span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
-    </a>
+    </a> -->
 </div>
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
