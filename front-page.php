6db1c2a35a9fdc6686d67009e0b17b7830219336
erikkowalski <erikkowalski@mac.com>
2015-06-08 16:16:41 -0400

    added arrows to carousel and grey bar underneath navbar on home page

--- front-page.php
+++ front-page.php
@@ -27,11 +27,11 @@
 
     <!-- Controls -->
     <a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
     <a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
 </div>
