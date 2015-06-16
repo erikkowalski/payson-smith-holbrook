58a8a929edac214cfe03a5287353fa7640312226
erikkowalski <erikkowalski@mac.com>
2015-06-16 15:00:10 -0400

    added Home page widget area

--- lib/init.php
+++ lib/init.php
@@ -68,5 +68,14 @@ function widgets_init() {
     'before_title'  => '<h3>',
     'after_title'   => '</h3>'
   ]);
+
+    register_sidebar([
+    'name'          => __('Home', 'sage'),
+    'id'            => 'sidebar-home',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ]);
 }
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
