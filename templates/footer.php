917b67b20a3364cb59bd43cbfee6f36245dbbed8
erikkowalski <erikkowalski@mac.com>
2015-06-17 16:41:32 -0400

    added wrapper to footer styling, adjusted layout on mobil device for footer nav

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,11 @@
 <footer class="content-info" role="contentinfo">
   <section class="row footer-top">
-		 <div class="col-md-4 footer-logo">
+		 <div class="col-xs-12 col-sm-4 footer-logo">
 			  <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/dist/images/psh-full.png" alt="">
 		 </div>
-		<nav class="">
+		<nav class="col-xs-12 col-sm-4 footer-nav">
 			 <?php if (has_nav_menu('footer_navigation')) :
-				wp_nav_menu(['theme_location' => 'footer_navigation',  'menu_class' => 'nav navbar-nav col-md-8']);
+				wp_nav_menu(['theme_location' => 'footer_navigation',  'menu_class' => 'nav navbar-nav ']);
 				endif; ?>
 		</nav>
 	 </section>
