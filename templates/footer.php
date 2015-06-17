e4f19d2a50a1050a678b636384bbd16c5d3bc1f8
erikkowalski <erikkowalski@mac.com>
2015-06-17 16:21:15 -0400

    cleaned up code formatting

--- templates/footer.php
+++ templates/footer.php
@@ -1,16 +1,18 @@
 <footer class="content-info" role="contentinfo">
   <section class="row footer-top">
-   <div class="col-md-4 footer-logo">
-       <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/dist/images/psh-full.png" alt="">
-   </div>
+		 <div class="col-md-4 footer-logo">
+			  <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/dist/images/psh-full.png" alt="">
+		 </div>
 		<nav class="">
 			 <?php if (has_nav_menu('footer_navigation')) :
-wp_nav_menu(['theme_location' => 'footer_navigation',  'menu_class' => 'nav navbar-nav col-md-8']);
-endif; ?>
+				wp_nav_menu(['theme_location' => 'footer_navigation',  'menu_class' => 'nav navbar-nav col-md-8']);
+				endif; ?>
 		</nav>
 	 </section>
+
 	 <section class="footer-bottom">
-		<div class="col-md-4">    <?php dynamic_sidebar('sidebar-footer'); ?>
+		<div class="col-md-4">
+			 <?php dynamic_sidebar('sidebar-footer'); ?>
 		</div>
 	 </section>
 </footer>
