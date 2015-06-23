9742cf84dd333ecb5ad490da31afb4274d5b7cc0
erikkowalski <erikkowalski@mac.com>
2015-06-23 14:27:39 -0400

    add font-samily-serif to global paragraphy styling

--- front-page.php
+++ front-page.php
@@ -44,29 +44,151 @@
 
 <section class="news-feed">
 
-<h2>Industry News</h2>
+	<h2>See what’s happening in your market and around the world.</h2>
 
-<?php //Display 3 random News Posts
-$args = [
-    'post_type' => 'psh_news',
+<?php //Display 3 random Costar News Posts
+$costar_feed_args = [
+    'post_type' => 'costar_news',
     'posts_per_page' => 3,
     'orderby' => 'rand'
 ];
 
-$testimonials = new WP_Query( $args );
+$costar_feed = new WP_Query( $costar_feed_args );
 ?>
    <aside  class="row">
-       <hr/>
 
-    <?php while ( $testimonials->have_posts() ) : $testimonials->the_post(); ?>
+       <hr/>
+		<h3>National Commercial Real Estate News:</h3>
+
+    <?php while ( $costar_feed->have_posts() ) : $costar_feed->the_post(); ?>
     <div class="col-md-4">
         <h3 class="entry-title"> <a href="<?php the_permalink(); ?>"><?php the_title();?> </a></h3>
         <div class="entry-content">
         <?php the_excerpt();?>
         </div>
     </div>
-    <?php endwhile;?>
+    <?php endwhile;
+		wp_reset_postdata();
+		?>
 </aside>
 
+
+
+<?php //Display 3 random Costar Boston News Posts
+$costar_boston_args = [
+	'post_type' => 'costar_boston',
+	'posts_per_page' => 3,
+	'orderby' => 'rand'
+];
+
+$costar_boston = new WP_Query( $costar_boston_args );
+	?>
+	<aside  class="row">
+		<hr/>
+		<h3>Boston Commercial Real Estate News</h3>
+		<?php while ( $costar_boston->have_posts() ) : $costar_boston->the_post(); ?>
+		<div class="col-md-4">
+			<h3 class="entry-title"> <a href="<?php the_permalink(); ?>"><?php the_title();?> </a></h3>
+			<div class="entry-content">
+				<?php the_excerpt();?>
+			</div>
+		</div>
+		<?php endwhile;?>
+	</aside>
+
+
+
+<?php //Display 3 random News Posts
+$m_and_a_args = [
+	'post_type' => 'm_and_a',
+	'posts_per_page' => 3,
+	'orderby' => 'rand'
+];
+
+
+$m_and_a = new WP_Query( $m_and_a_args ); ?>
+	<aside  class="row">
+		<hr/>
+		<h3>M&amp;A News: </h3>
+		<?php while ( $m_and_a->have_posts() ) : $m_and_a->the_post(); ?>
+		<div class="col-md-4">
+			<h3 class="entry-title"> <a href="<?php the_permalink(); ?>"><?php the_title();?> </a></h3>
+			<div class="entry-content">
+				<?php the_excerpt();?>
+			</div>
+		</div>
+		<?php endwhile;?>
+	</aside>
+
+	<?php //Display 3 random News Posts
+$business_args = [
+	'post_type' => 'business',
+	'posts_per_page' => 3,
+	'orderby' => 'rand'
+];
+
+
+$business_news = new WP_Query( $business_args ); ?>
+	<aside  class="row">
+		<hr/>
+		<h3>Business News:</h3>
+		<?php while ( $business_news->have_posts() ) : $business_news->the_post(); ?>
+		<div class="col-md-4">
+			<h3 class="entry-title"> <a href="<?php the_permalink(); ?>"><?php the_title();?> </a></h3>
+			<div class="entry-content">
+				<?php the_excerpt();?>
+			</div>
+		</div>
+		<?php endwhile;?>
+	</aside>
+
+	<?php //Display 3 random News Posts
+$business_args = [
+	'post_type' => 'economic',
+	'posts_per_page' => 3,
+	'orderby' => 'rand'
+];
+
+
+$business_news = new WP_Query( $business_args ); ?>
+	<aside  class="row">
+		<hr/>
+		<h3>Economic News:</h3>
+		<?php while ( $business_news->have_posts() ) : $business_news->the_post(); ?>
+		<div class="col-md-4">
+			<h3 class="entry-title"> <a href="<?php the_permalink(); ?>"><?php the_title();?> </a></h3>
+			<div class="entry-content">
+				<?php the_excerpt();?>
+			</div>
+		</div>
+		<?php endwhile;?>
+	</aside>
+
+	<?php //Display 3 random News Posts
+$business_args = [
+	'post_type' => 'financial',
+	'posts_per_page' => 3,
+	'orderby' => 'rand'
+];
+
+
+$business_news = new WP_Query( $business_args ); ?>
+	<aside  class="row">
+		<hr/>
+		<h3>Financial News:</h3>
+		<?php while ( $business_news->have_posts() ) : $business_news->the_post(); ?>
+		<div class="col-md-4">
+			<h3 class="entry-title"> <a href="<?php the_permalink(); ?>"><?php the_title();?> </a></h3>
+			<div class="entry-content">
+				<?php the_excerpt();?>
+			</div>
+		</div>
+		<?php endwhile;?>
+	</aside>
+
+</section>
+
+<section>
+	<p>Payson Smith Holbrook, Inc. specializes in Acquisitions &amp; Dispositions, Agency Leasing, Project Management and Tenant Representation.  We at PSH work every day to develop the interpersonal relationships with our clients that result in a  deeper understanding of their present and future expectations. Whether your needs are the expansion into a new market, the disposition of non-productive assets or the oversight of a complex project, we are here to help navigate through the times. PSH is dedicated to committing our team to be a part of your team in consultative planning, build-to-suit design, market and demographic evaluation, true cost of occupancy analysis, project management, property management and lease administration. </p>
 </section>
 
