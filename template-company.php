5e2cce93286f538982f1242ee6b0acf26dc7a9fe
erikkowalski <erikkowalski@mac.com>
2015-07-02 14:04:40 -0400

    initial commit

---
+++ template-company.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Company Template
+ */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
