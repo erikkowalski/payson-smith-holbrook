3b7873849b428548d13256e05ba00a12b8a4ec37
erikkowalski <erikkowalski@mac.com>
2015-07-03 14:45:25 -0400

    add custom fields to contact tempate, added .company class to company template

--- page-contact.php
+++ page-contact.php
@@ -1,5 +1,14 @@
-<h1>test</h1>
+
 <?php while (have_posts()) : the_post(); ?>
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+
+<div class="container">
+<div class="col-md-5 info">
+<?php the_field('info'); ?>
+</div>
+<div class="col-md-7 form">
+<?php the_field('form'); ?>
+</div>
+</div>
--- template-company.php
+++ template-company.php
@@ -3,8 +3,11 @@
  * Template Name: Company Template
  */
 ?>
+<section class="company">
 
 <?php while (have_posts()) : the_post(); ?>
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+
+</section>
