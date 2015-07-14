8348dfa5c91848494113571b4267dcbccd0804a0
erikkowalski <erikkowalski@mac.com>
2015-07-14 16:14:21 -0400

    added container to google map iframe on contact page, added aside and content to our performace

--- page-contact.php
+++ page-contact.php
@@ -3,9 +3,10 @@
 <?php get_template_part('templates/page', 'header'); ?>
 <h1 class="tittle">Contact</h1>
 
+<div class="container">
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
-
+</div>
 <div class="container">
 <div class="col-md-5 info">
 <?php the_field('info'); ?>
