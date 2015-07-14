7483d24c867c1a7a02689990f9934c4b8fdbe2fe
erikkowalski <erikkowalski@mac.com>
2015-07-14 17:18:28 -0400

    added styling to our performace page

--- page-our-performance.php
+++ page-our-performance.php
@@ -1,20 +1,16 @@
-<iframe id="iframeWidget" scrolling="no" overflow="hidden" style="HEIGHT:450px; WIDTH: 100%" src="https://batchgeo.com/map/a886653cfddb03d11de79a99abb3a408" frameborder="0" allowtransparency=""></iframe>
+<iframe class="hidden-xs" id="iframeWidget" scrolling="no" overflow="hidden" style="HEIGHT:450px; WIDTH: 100%" src="https://batchgeo.com/map/a886653cfddb03d11de79a99abb3a408" frameborder="0" allowtransparency=""></iframe>
 	<section class="company container">
 
 
-	<?php while (have_posts()) : the_post(); ?>
-	<?php get_template_part('templates/page', 'header'); ?>
-	<div class="content">
-		<?php get_template_part('templates/content', 'page'); ?>
-	</div>
-	<?php endwhile; ?>
 
+		<?php while (have_posts()) : the_post(); ?>
+		<?php get_template_part('templates/page', 'header'); ?>
 <div class="col-md-8">
-	<img class="pull-left" src="<?php bloginfo('template_directory'); ?>/dist/images/3d-map.jpeg" alt="3D Map">
+	<img class="pull-left map" src="<?php bloginfo('template_directory'); ?>/dist/images/3d-map.jpeg" alt="3D Map">
 		<h3>OVER 8 MILLION SQUARE FEET LEASED IN THE PAST FIVE YEARS</h3>
 		<p>Navigating the every changing world of commercial real estate is difficult and complicated – but the professionals at Payson Smith Holbrook Inc. are here to help you</p>
 		<p>After ten years, PSH has developed an amazing team of brokers, agents, consultants, managers and maintenance personnel who help small-, medium-, and large-sized business move the economy forward every single day. We've done it all and made sure everything went smoothly in the process.</p>
-		<div class="new-build">
+		<div class="new-build clearfix">
 			<img class="pull-left" src="<?php bloginfo('template_directory'); ?>/dist/images/crane.jpeg" alt="new construction">
 			<h3>New-Build Construction</h3>
 			<ul>
@@ -24,14 +20,16 @@
 				<li>Build</li>
 			</ul>
 		</div>
-
+<div class="last-p">
 			<p>Sometimes, no space completely matches your vision. No problem. Look into a new-build solution. Whether this means demoing the entire property, or merely a deep clean, Payson Smith Holbrook Inc. has the connections and resources to make your vision come to life.</p>
+	</div>
 		</div>
+
+
 		<aside class="col-md-4">
-			<p>We work with small-, medium-, and large-sized businesses to help them either get started or expand into new territories. Start your next chapter now.</p>
-			<p>If you have a portfolio of properties you're moving, or if you have just a single spot, we'll connect you with potential buyers.</p>
-			<p>
-				No business is an island – we know that better than anyone. We're always looking for complementary services to offer our clients.
-			</p>
+			<div class="content ">
+				<?php get_template_part('templates/content', 'page'); ?>
+			</div>
+			<?php endwhile; ?>
 		</aside>
 </section>
