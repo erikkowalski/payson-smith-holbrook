97ed604f0e773629379fb637c2a61953dfd96cec
erikkowalski <erikkowalski@mac.com>
2015-07-20 14:02:53 -0400

    made footer logo smaller, updated styling on compay template

--- template-company.php
+++ template-company.php
@@ -3,8 +3,11 @@
  * Template Name: Company Template
  */
 ?>
-<section class="company">
 
+<div class="company-header">
+	<img class="img-responsive" src="<?php the_field('company_header_img') ?>" alt="Header Image">
+</div>
+<section class="company">
 <?php while (have_posts()) : the_post(); ?>
 <?php get_template_part('templates/page', 'header'); ?>
 	<div class="content">
