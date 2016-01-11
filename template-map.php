86e98504caa6d69cc5bc20431f5e248448351a1b
erikkowalski <erikkowalski@mac.com>
2016-01-11 12:26:58 -0500

    added map for mobil layout, incresaed height on desktop layout

--- template-map.php
+++ template-map.php
@@ -8,3 +8,7 @@
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+<iframe class="visible-xs" id="iframeWidget" scrolling="no" overflow="hidden" style="HEIGHT:350px; WIDTH: 100%" src="https://batchgeo.com/map/a886653cfddb03d11de79a99abb3a408" frameborder="0" allowtransparency=""></iframe>
+
+<iframe class="hidden-xs" id="iframeWidget" scrolling="no" overflow="hidden" style="HEIGHT:650px; WIDTH: 100%" src="https://batchgeo.com/map/a886653cfddb03d11de79a99abb3a408" frameborder="0" allowtransparency=""></iframe>
+
