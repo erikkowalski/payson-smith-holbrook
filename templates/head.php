35062429e449660172deaf19185f365348a1df17
erikkowalski <erikkowalski@mac.com>
2019-11-20 16:39:09 -0500

    updated font call

--- templates/head.php
+++ templates/head.php
@@ -1,8 +1,8 @@
 <head>
+ <link rel="stylesheet" href="https://use.typekit.net/ijc8zah.css">
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="//use.typekit.net/ijc8zah.js"></script>
-    <script>try{Typekit.load();}catch(e){}</script>
+
   <?php wp_head(); ?>
 </head>
